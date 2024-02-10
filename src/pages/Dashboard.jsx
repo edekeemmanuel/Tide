3cc1b00e3c31fce20d288e0a1fd79d5e1d10524e
edekeemmanuel <edekeemmanuel55@gmail.com>
2024-02-10 17:57:50 +0100

    dev tide

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,6 @@
 import Navbar from '../component/dashboard/Navbar.jsx';
 import Courses from "../component/dashboard/Course"
-//import Card from "./Card"
-//import TopCategories from "./TopCategories"
+
 const Dashboard = () => {
 return (
   <>
