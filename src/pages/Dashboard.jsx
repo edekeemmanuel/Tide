fea9b9147eb7769ca96ed7ceacfe95bfd36e0253
edekeemmanuel <edekeemmanuel55@gmail.com>
2024-02-10 17:50:21 +0100

    dev

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import Navbar from '../component/dashboard/Navbar.jsx';
 import Courses from "../component/dashboard/Course"
 //import Card from "./Card"
-import TopCategories from "./TopCategories"
+//import TopCategories from "./TopCategories"
 const Dashboard = () => {
 return (
   <>
