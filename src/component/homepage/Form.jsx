7ab9af15c0ea734d7cdcced33d4299784c1447c4
edekeemmanuel <edekeemmanuel55@gmail.com>
2024-02-12 18:07:30 +0100

    signin tides

--- src/component/homepage/Form.jsx
+++ src/component/homepage/Form.jsx
@@ -4,7 +4,7 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { Link } from 'react-router-dom';
 
 const Forms = (props) => (
-  const [name, setName] = useState(false)
+  //const [name, setName] = useState(false)
   <div className="">
     <Formik
       initialValues={{ email: '', password: '' }}
@@ -28,9 +28,6 @@ const Forms = (props) => (
     >
       {({ isSubmitting }) => (
         <Form>
-          {
-            
-          }
           <Field className="border-2 w-full hover:border-neutral-500 border-neutral-500 bg-transparent py-[10px] focus:border-neutral-50 focus-within:border-neutral-50 px-3 w-full placeholder:text-sm placeholder:text-neutral-50" type="email" name="email" placeholder="Email" />
           <ErrorMessage name="email" component="div" />
           <Field className="mt-5 border-2 w-full hover:border-neutral-500 border-neutral-500 bg-transparent  py-[10px] focus:border-neutral-50 focus-within:border-neutral-50 px-3 w-full placeholder:text-sm placeholder:text-neutral-50" type="password" name="password" placeholder="Password"/>
